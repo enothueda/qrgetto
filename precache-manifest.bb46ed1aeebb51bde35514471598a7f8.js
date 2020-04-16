269d45ebde5fd1e25c24c40a547092d67c94708c
Enot Hueda <enot.hueda@gmail.com>
2020-04-16 16:40:50 -0500

    Updates

---
+++ precache-manifest.bb46ed1aeebb51bde35514471598a7f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a8b9ae3497e4d577c6f132b941ae4a4",
+    "url": "/qrgetto/index.html"
+  },
+  {
+    "revision": "a4c7bedf9b638f32e0b7",
+    "url": "/qrgetto/static/css/main.8149aa9e.chunk.css"
+  },
+  {
+    "revision": "fd50085179ef54d85094",
+    "url": "/qrgetto/static/js/2.00baeb16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/qrgetto/static/js/2.00baeb16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4c7bedf9b638f32e0b7",
+    "url": "/qrgetto/static/js/main.060f2240.chunk.js"
+  },
+  {
+    "revision": "f4470aab6939b7437364",
+    "url": "/qrgetto/static/js/runtime-main.007ee878.js"
+  }
+]);
